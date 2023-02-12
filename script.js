91cf1164a7b369f88d5945a675c5f93fb9b08df8
serenakang <serenakang77@gmail.com>
2023-02-11 23:09:20 -0500

    Added dropdown function

--- script.js
+++ script.js
@@ -1,82 +1,32 @@
-const app = {};
+const app = {}
 
-app.apiKey = "1";
+app.apiKey = "1"
+const $dropdown = $("#dropdown")
 
-const $dropdown = $("#dropdown");
-const $alcoholPicture = $("#alcoholPicture");
-
-// This function populates dropdown
-app.populateDropdown = function(){
-    $.ajax({
-        url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a",
-        method: "GET",
-        dataType: "json",
-        data: {
-            api_key: app.apiKey
-        } 
-    }).then(function(response){        
-        response.drinks.forEach(function(data){
-            const cocktailName = data.strDrink;
-            const cocktailId = data.idDrink; 
-            const htmlToAppend = `
-                <option hidden disabled selected value>-- Select your Cocktail --</option>
-                <option class="cocktailName" value ="${cocktailId}">${cocktailName}</option>
-            `;
-            if(data.strIngredient4 && data.strMeasure4){
-                $dropdown.append(htmlToAppend);
-            }
-        })         
-        })     
-}
-// This function gets the value that we selected
-app.getSelectValue = function(){
-    $dropdown.on("change", function(){
-        const selection = $('option:selected').val();
-        $alcoholPicture.empty();
-        app.getCocktail(selection);
+app.populateDropdown = function () {
+  $.ajax({
+    url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c",
+    method: "GET",
+    dataType: "json",
+    data: {
+      api_key: app.apiKey,
+    },
+  }).then(function (response) {
+    response.drinks.forEach((individual) => {
+      $dropdown.append(
+        `
+        <option hidden disabled selected value>Please select cocktail</option>
+        <option value=${individual.strDrink}>${individual.strDrink}</option>
+        `
+      )
     })
-}
-// This function allows us to choose 1 cocktail by id
-app.getCocktail = function(cocktailId){
-    $.ajax ({
-        url: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`,
-        method: "GET",
-        dataType: "json",
-        data: {
-            api_key: app.apiKey
-        }
-    }).then(function(response){
-        app.displayCocktail(response);
-    })  
-}    
-// This function allows us to show image, name, ingredient, and instruction
-app.displayCocktail = function(data){
-    const htmltoAppend2 = `
-        <div class="imageClass">
-            <img class="cocktailImage" src="${data.drinks[0].strDrinkThumb}" alt="Cocktail">
-        </div>
-        <div class="divide">
-            <div class="nameClass">    
-                <h2>🍸Name: ${data.drinks[0].strDrink}</h2>
-            </div>    
-            <div class="ingredientClass">    
-                <h2>🍸Ingredient: ${data.drinks[0].strIngredient1}( ${data.drinks[0].strMeasure1}), ${data.drinks[0].strIngredient2}( ${data.drinks[0].strMeasure2}), ${data.drinks[0].strIngredient3}( ${data.drinks[0].strMeasure3}), ${data.drinks[0].strIngredient4}( ${data.drinks[0].strMeasure4})</h2>
-            </div>    
-            <div class="instructionClass">    
-                <h2>🍸Instruction: ${data.drinks[0].strInstructions}</h2>
-            </div>
-        </div>        
-        `;
-    $alcoholPicture.append(htmltoAppend2);
+  })
 }
 
-app.init = function(){
-    app.getCocktail("17222");
-    app.populateDropdown();
-    app.getCocktail();
-    app.getSelectValue();
+app.init = function () {
+  app.populateDropdown()
 }
 
-$(function(){
-    app.init();
+$(function () {
+  app.init()
 })
